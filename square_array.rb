26d38eb585696a79ec1a15f600d2dcc8a3971463
Nory Lazo <nlazo@icloud.com>
2019-11-08 05:13:44 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,8 +2,8 @@ def square_array(array)
   # your code here
   array_squared = []
   
-  # array.each do |i|
-  #   array_squared << i**2
-  # end
-  # array_squared
+  array.each do |i|
+    array_squared << i**2
+  end
+  array_squared
 end
